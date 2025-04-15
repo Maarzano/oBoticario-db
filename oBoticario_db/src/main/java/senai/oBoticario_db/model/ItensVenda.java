6e4213f7862bc5f73932713c066243ab969f90c9
JoãoLucas <joaolucascosta32@gmail.com>
2025-04-14 21:15:35 -0300

    criaçao da tabela itens venda

--- oBoticario_db/src/main/java/senai/oBoticario_db/model/ItensVenda.java
+++ oBoticario_db/src/main/java/senai/oBoticario_db/model/ItensVenda.java
@@ -5,7 +5,6 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "Itens_Venda")
@@ -15,16 +14,22 @@ public class ItensVenda {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ItenVendaID")
+    @Column(name = "ItemVendaID")
     private long id;
 
     @ManyToOne
     @JoinColumn(name = "VendaID", referencedColumnName = "VendaID", nullable = false)
-    private Vendas venda;
+    private Vendas vendaID;
 
     @ManyToOne
-    @JoinColumn(name = "Produto", referencedColumnName = "LojaID", nullable = false)
-    private Lojas loja;
+    @JoinColumn(name = "ProdutoID_Loja", referencedColumnName = "ProdutoID_Loja", nullable = false)
+    private Lojas lojaID;
+
+    @Column(name = "Quantidade")
+    private long Quantidade;
+
+    @Column(name = "Preco_Unitario")
+    private BigDecimal Preco_Unitario;
 
 
 }
