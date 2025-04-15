313146f39ca5ebd2769b030fe4ceb38b9543c443
Marzano <arthuramarzano10@gmail.com>
2025-04-15 10:17:03 -0300

    colocando anotações repository

--- oBoticario_db/src/main/java/senai/oBoticario_db/Repository/FuncDistribuidoraRepository.java
+++ oBoticario_db/src/main/java/senai/oBoticario_db/Repository/FuncDistribuidoraRepository.java
@@ -1,7 +1,10 @@
 package senai.oBoticario_db.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import senai.oBoticario_db.model.FuncDistribuidora;
 
+@Repository
 public interface FuncDistribuidoraRepository extends JpaRepository<FuncDistribuidora, Long> {
 }
--- oBoticario_db/src/main/java/senai/oBoticario_db/Repository/ProdDistribuidoraRepository.java
+++ oBoticario_db/src/main/java/senai/oBoticario_db/Repository/ProdDistribuidoraRepository.java
@@ -1,7 +1,10 @@
 package senai.oBoticario_db.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import senai.oBoticario_db.model.ProdDistribuidora;
 
+@Repository
 public interface ProdDistribuidoraRepository extends JpaRepository<ProdDistribuidora, Long> {
 }
